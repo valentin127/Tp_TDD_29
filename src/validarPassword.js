1b9309444cf28613e2ff56f1e1382007378b11b4
Valentin Michelic <valentinmichelic@gmail.com>
2026-04-29 20:19:36 -0300

    Ciclo 2
    ciclo 2 - validar longitud minima. Agrego la funcionalidad de la longitud minima y verde.

--- src/validarPassword.js
+++ src/validarPassword.js
@@ -1,5 +1,11 @@
 function validarPassword(password) {
-    return { esValida: true, errores: [] };
+    const errores = [];
+
+    if (password.length < 8) {
+        errores.push('Debe tener al menos 8 caracteres');
+    }
+
+    return { esValida: errores.length === 0, errores };
 }
 
 module.exports = { validarPassword };
